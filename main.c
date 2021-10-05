e76ff1a8cccc1a410aff101f7e985e5d277f2188
matvienko <73351167+girsammatvienko@users.noreply.github.com>
2021-10-05 19:05:36 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include "arraylist.h"
+
+int main() {
+    int a = 13, b = 20, c = 22, cc = 66, x = 0, y = 43, i;
+    srand(time(0));
+
+    alist_t* intlist = initializeList(sizeof(int));
+    addElementInList(intlist, &a);
+    addElementInList(intlist, &b);
+    addElementInList(intlist, &c);
+    insertElementInList(intlist, &cc, 3);
+    setElementByIndex(intlist, &y, 1);
+
+    assert(intlist->size == sizeof(int));
+    assert(13 == *(int*) getElement(intlist, 0));
+    assert(43 == *(int*) getElement(intlist, 1));
+    assert(22 == *(int*) getElement(intlist, 2));
+    assert(66 == *(int*) getElement(intlist, 3));
+
+    for(i = 0;i < getSize(intlist);i++) {
+        printf("%d ", *(int*)getElement(intlist, i));
+    }
+}
